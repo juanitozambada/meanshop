6ad53c8446c5d2571b903b92315adc518700679e
juan <openarms_@hotmail.com>
2016-06-17 09:41:04 -0600

    Wiring the product controller with RESTful methods

--- client/app/products/products.controller.js
+++ client/app/products/products.controller.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var errorHandler;
+
 angular.module('meanshopApp')
 	.controller('ProductsCtrl', function($scope, Product) {
 		$scope.products = Product.query();
@@ -9,24 +11,34 @@ angular.module('meanshopApp')
 		$scope.product = Product.get({id: $stateParams.id});
 
 		$scope.deleteProduct = function() {
-			Product.delete($scope.product);
+			Product.delete({id: $scope.product._id}, function success() {
+				$state.go('products');
+			}, errorHandler($scope));
 			$state.go('products');
-		}
+		};
 	})
 
 	.controller('ProductNewCtrl', function($scope, $state, Product) {
 		$scope.product = {}; // create a new instance
-		$scope.addProduct = function(product) {
-			Product.create($scope.product);
-			$state.go('products');
-		}
+		$scope.addProduct = function() {
+			Product.save($scope.product, function success(value) {
+				$state.go('viewProduct', {id: value._id});
+			}, errorHandler($scope));
+		};
 	})
 
 	.controller('ProductEditCtrl', function($scope, $state, $stateParams, Product) {
 		$scope.product = Product.get({id: $stateParams.id});
 
-		$scope.editProduct = function(product) {
-			Product.update($scope.product);
-			$state.go('products');
-		}
+		$scope.editProduct = function() {
+			Product.update({id: $scope.product._id}, $scope.product, function success(value) {
+				$state.go('viewProduct', {id: value._id});
+			}, errorHandler($scope));
+		};
 	});
+
+errorHandler = function($scope) {
+	return function error(httpResponse) {
+		$scope.errors = httpResponse;
+	};
+};
